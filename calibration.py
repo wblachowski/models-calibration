b450b5b691d88ac395d9ffac8134a6b8532851bc
Wojciech Blachowski <wblachowsky@gmail.com>
2021-09-01 23:24:12 +0200

    Refactoring

--- calibration.py
+++ calibration.py
@@ -5,19 +5,18 @@ from sklearn.isotonic import IsotonicRegression
 
 class PlattCalibrator:
     def __init__(self, prob_pred, prob_true):
-        prob_pred_2, prob_true_2 = list(
-            zip(*[p for p in zip(prob_pred, prob_true) if 0 < p[1] < 1])
-        )
-        prob_true_2 = np.log((1 / np.array(prob_true_2)) - 1)
-        prob_pred_2 = np.array(prob_pred_2)
-        model = LinearRegression().fit(
-            prob_pred_2.reshape(-1, 1), prob_true_2.reshape(-1, 1)
-        )
+        prob_pred, prob_true = self._filter_out_of_domain(prob_pred, prob_true)
+        prob_true = np.log((1 / np.array(prob_true)) - 1).reshape(-1, 1)
+        prob_pred = np.array(prob_pred).reshape(-1, 1)
+        model = LinearRegression().fit(prob_pred, prob_true)
         self.alpha = model.coef_[0, 0]
         self.beta = model.predict([[0]])[0, 0]
 
     def calibrate(self, probabilities):
         return 1 / (1 + np.exp(self.alpha * np.array(probabilities) + self.beta))
+
+    def _filter_out_of_domain(self, prob_pred, prob_true):
+        return list(zip(*[p for p in zip(prob_pred, prob_true) if 0 < p[1] < 1]))
 
 
 class IsotonicCalibrator:
