bd6f1b6525b1f87e5ea3ae1b286231de4b7c0e4a
Wojciech Blachowski <wblachowsky@gmail.com>
2021-09-19 20:29:36 +0200

    Refactor calibration

--- calibration.py
+++ calibration.py
@@ -6,15 +6,17 @@ from sklearn.linear_model import LinearRegression
 class SigmoidCalibrator:
     def __init__(self, prob_pred, prob_true):
         prob_pred, prob_true = self._filter_out_of_domain(prob_pred, prob_true)
-        prob_true = np.log((1 / np.array(prob_true)) - 1).reshape(-1, 1)
-        prob_pred = np.array(prob_pred).reshape(-1, 1)
-        self.regressor = LinearRegression().fit(prob_pred, prob_true)
+        prob_true = np.log(prob_true / (1 - prob_true))
+        self.regressor = LinearRegression().fit(
+            prob_pred.reshape(-1, 1), prob_true.reshape(-1, 1)
+        )
 
     def calibrate(self, probabilities):
-        return 1 / (1 + np.exp(self.regressor.predict(probabilities.reshape(-1, 1))))
+        return 1 / (1 + np.exp(-self.regressor.predict(probabilities.reshape(-1, 1))))
 
     def _filter_out_of_domain(self, prob_pred, prob_true):
-        return list(zip(*[p for p in zip(prob_pred, prob_true) if 0 < p[1] < 1]))
+        filtered = list(zip(*[p for p in zip(prob_pred, prob_true) if 0 < p[1] < 1]))
+        return np.array(filtered)
 
 
 class IsotonicCalibrator:
