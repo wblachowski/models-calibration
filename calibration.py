2547c84a247f3471895c7cb376df293f6ac65cce
Wojciech Blachowski <wblachowsky@gmail.com>
2021-09-19 19:54:27 +0200

    Refactor sigmoid calibrator

--- calibration.py
+++ calibration.py
@@ -8,12 +8,10 @@ class SigmoidCalibrator:
         prob_pred, prob_true = self._filter_out_of_domain(prob_pred, prob_true)
         prob_true = np.log((1 / np.array(prob_true)) - 1).reshape(-1, 1)
         prob_pred = np.array(prob_pred).reshape(-1, 1)
-        model = LinearRegression().fit(prob_pred, prob_true)
-        self.alpha = model.coef_[0, 0]
-        self.beta = model.predict([[0]])[0, 0]
+        self.regressor = LinearRegression().fit(prob_pred, prob_true)
 
     def calibrate(self, probabilities):
-        return 1 / (1 + np.exp(self.alpha * np.array(probabilities) + self.beta))
+        return 1 / (1 + np.exp(self.regressor.predict(probabilities.reshape(-1, 1))))
 
     def _filter_out_of_domain(self, prob_pred, prob_true):
         return list(zip(*[p for p in zip(prob_pred, prob_true) if 0 < p[1] < 1]))
